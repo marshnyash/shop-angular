4bdb43359800609c6f7a9351ba8f45bbedd57590
Anastasiya <marshyna.anastasiya@gmail.com>
2020-01-25 10:24:49 +0000

    created cart, product and first cmps

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first/first-component/first-component/first-component.component';
+import { CartComponent } from './components/cart/cart/cart.component';
+import { ProductsService } from './components/products/products.service';
+import { ProductsModule } from './components/products/products.modul';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, FirstComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule, ProductsModule],
+  providers: [ProductsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
